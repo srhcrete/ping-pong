50dcc4f32a25d7b56a2c92a1c0a37d1e10c6436b
Guest User <Guest@Epicodus-8B.local>
2017-08-18 16:55:55 -0700

    add css styles

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,29 @@
   //**function numberArray pushes inegers from 1 up to the input number into empty array and applies pingPong function to each item in the array
 var arrs = [];
 function numberArray(n) {
-for (var i=1; i <= n; i++) {
-  arrs.push(i);
-  pingPong(i);
-}
+  //$('#numberList ul').empty(); trying to empty div first when numberArray function is called
+  for (var i=1; i <= n; i++) {
+    arrs.push(i);
+    pingPong(i);
+  }
 };
 
   //**function pingPong checks each item in a array for a ping-pong number case, applies the rule for the case, and prints the results to <ul>
 var resultsArrs = [];
 function pingPong(i) {
-  if (i % 15 === 0) {
-    i = 'Ping-Pong';
-    $('#numberList').append('<li>' + i + '</li>');
-  } else if (i % 3 === 0) {
-      i = 'Ping';
+  //$('#numberList ul').empty(); trying to empty div first when pingPong function is called
+    if (i % 15 === 0) {
+      i = 'Ping-Pong';
       $('#numberList').append('<li>' + i + '</li>');
-    } else if (i % 5 === 0) {
-      i = 'Pong';
-      $('#numberList').append('<li>' + i + '</li>');
-        } else {
-          $('#numberList').append('<li>' + i + '</li>');
-          }
+    } else if (i % 3 === 0) {
+        i = 'Ping';
+        $('#numberList').append('<li>' + i + '</li>');
+      } else if (i % 5 === 0) {
+        i = 'Pong';
+        $('#numberList').append('<li>' + i + '</li>');
+          } else {
+            $('#numberList').append('<li>' + i + '</li>');
+            }
 };
 
 //User Interface Logic
@@ -32,7 +34,6 @@ $(document).ready(function() {
     event.preventDefault();
     var n = $('#userInputNumber').val();
     numberArray(n);
-
     $( "#refresh" ).click(function() {
    location.reload(true);
    });
